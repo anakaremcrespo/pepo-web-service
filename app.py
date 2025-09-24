49742ea2c16fc5f07cc15cf717a722aa67b9a755
Ana Karem Crespo López <karem_crespo@hotmail.com>
2025-09-23 20:04:25 -0700

    Agregar diseño

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hola():
-    return jsonify({"mensaje": "Hola Mundo, bienvenido a Pepo Web Service"})
+    return """
+    <html>
+        <head>
+            <title>Pepo Web Service</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f0f8ff;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                    text-align: center;
+                }
+                h1 {
+                    color: #2e8b57;
+                    font-size: 36px;
+                    margin: 0;
+                }
+                p {
+                    color: #333;
+                    font-size: 24px;
+                    margin: 10px 0 0 0;
+                }
+            </style>
+        </head>
+        <body>
+            <div>
+                <h1>Hola Mundo, bienvenido a Pepo Web Service</h1>
+                <p>Ana Karem Crespo López - 22031437</p>
+            </div>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
