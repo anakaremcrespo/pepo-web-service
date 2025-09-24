ccdf8ffba63167781ce86893350c1d7be67925a9
Ana Karem Crespo López <karem_crespo@hotmail.com>
2025-09-23 19:53:39 -0700

    Agregar texto al Hola Mundo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hola():
-    return jsonify({"mensaje": "Hola Mundo"})
+    return jsonify({"mensaje": "Hola Mundo, bienvenido a Pepo Web Service"})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
